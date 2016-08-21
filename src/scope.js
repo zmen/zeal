62d84d24c78487ab759dce35bb2bf9413eca002a
zmen <huzhiminszlx@163.com>
2016-08-21 20:43:16 +0800

    Add $watch and $digest to Scope

--- src/scope.js
+++ src/scope.js
@@ -1,6 +1,21 @@
 /* jshint globalstrict: true */
 /* global module: false */
 "use strict";
-function Scope() {}
+function Scope() {
+    this.watchers = [];
+}
+
+Scope.prototype.$watch = function (watcherFn, listenerFn) {
+    this.watchers.push({
+        watcherFn: watcherFn,
+        listenerFn: listenerFn
+    });
+};
+
+Scope.prototype.$digest = function () {
+    this.watchers.forEach(function (v) {
+        v.listenerFn();
+    });
+}
 
 module.exports.Scope = Scope;
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -3,6 +3,8 @@
 /* global it: false */
 /* global expect: false */
 /* global require: false */
+/* global beforeEach: false */
+/* global jasmine: false */
 "use strict"; 
 
 var Scope = require('../src/scope').Scope;
@@ -14,5 +16,23 @@ describe("Scope", function () {
         scope.aProperty = 1;
         
         expect(scope.aProperty).toBe(1);
+    });
+
+    describe("digest", function () {
+        var scope;
+
+        beforeEach(function () {
+            scope = new Scope();
+        });
+
+        it("calls the listener function of a watch on first $digest", function () {
+            var watchFn = function () { return 'wat'; };
+            var listenerFn = jasmine.createSpy();
+            scope.$watch(watchFn, listenerFn);
+
+            scope.$digest();
+
+            expect(listenerFn).toHaveBeenCalled();
+        });
     });
 });

06b1357370209c9883f87a729f5ed8da7162564c
zmen <huzhiminszlx@163.com>
2016-09-12 20:07:37 +0800

    finish watchFn for object

--- src/scope.js
+++ src/scope.js
@@ -296,9 +296,11 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
     var self = this;
     var newValue;
     var oldValue;
+    var oldLength;
     var changeCount = 0;
 
     var internalWatchFn = function (scope) {
+        var newLength;
         newValue = watchFn(scope);
 
         if (_.isObject(newValue)) {
@@ -322,7 +324,33 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
                 if (!_.isObject(oldValue) || _.isArrayLike(oldValue)) {
                     changeCount++;
                     oldValue = {};
-                } 
+                    oldLength = 0;
+                }
+                newLength = 0;
+                _.forOwn(newValue, function (newVal, key) {
+                    newLength++;
+                    if (oldValue.hasOwnProperty(key)) {
+                        var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+                        if (!bothNaN && oldValue[key] !== newVal) {
+                            changeCount++;
+                            oldValue[key] = newVal;
+                        }
+                    } else {
+                        changeCount++;
+                        oldLength++;
+                        oldValue[key] = newVal;
+                    }
+                });
+                if (oldLength > newLength) {
+                    changeCount++;
+                    _.forOwn(oldValue, function (oldVal, key) {
+                        if (!newValue.hasOwnProperty(key)) {
+                            oldLength--;
+                            delete oldValue[key];
+                        }
+                    });
+                }
+
             }
         } else {
             if (!self.$$areEqual(newValue, oldValue, false)) {
